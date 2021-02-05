13c9d26e1d5878fbdab0dc9be452b44801d9deb1
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 16:39:12 +0100

    Update Block.cpp

--- Source/Block.cpp
+++ Source/Block.cpp
@@ -1,1 +1,34 @@
+#include "./Block.h"
 
+
+Block::Block()
+{
+    this->block_type = 0;
+    this->block_code = 0;
+}
+
+Block::Block(int t, int c)
+{
+    this->block_type = t;
+    this->block_code = c;
+}
+
+int Block::GetType()
+{
+    return this->block_type;
+}
+
+int Block::GetCode()
+{
+    return this->block_code;
+}
+
+void Block::SetType(int t)
+{
+    this->block_type = t;
+}
+
+void Block::SetCode(int c)
+{
+    this->block_code = c;
+}
