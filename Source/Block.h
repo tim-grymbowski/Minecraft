ba7292cec46e4bf485ac9605c1e30d691e11aae4
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 16:40:42 +0100

    Update Block.h

--- Source/Block.h
+++ Source/Block.h
@@ -1,1 +1,17 @@
+#pragma once
 
+class Block
+{
+private:
+    int block_type;
+    int block_code;
+public:
+    Block();
+    Block(int t, int c);
+
+    int GetType();
+    int GetCode();
+
+    void SetType(int t);
+    void SetCode(int c);
+};
