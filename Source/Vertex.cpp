63c4f76bd0649c3f9871a29967bfa87b6fbb5fc8
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:17:36 +0100

    Update Vertex.cpp

--- Source/Vertex.cpp
+++ Source/Vertex.cpp
@@ -1,1 +1,22 @@
+#include "./Vertex.h"
 
+
+Vertex::Vertex()
+{
+	this->x = +0.0f;
+	this->y = +0.0f;
+	this->z = +0.0f;
+	this->u = +0.0f;
+	this->v = +0.0f;
+	this->t = -1.0f;
+}
+
+Vertex::Vertex(float x, float y, float z, float u, float v, float t)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->u = u;
+	this->v = v;
+	this->t = t;
+}
