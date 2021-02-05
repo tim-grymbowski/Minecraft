870fa360640a9c05f32165991419c76e86480f4c
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:13:47 +0100

    Update Texture.h

--- Source/Texture.h
+++ Source/Texture.h
@@ -1,1 +1,17 @@
+#pragma once
 
+
+class Texture
+{
+private:
+	unsigned int RendererID;
+	unsigned int slot;
+public:
+	Texture(const char* path, int width, int height, int bits_per_pixel, int slot);
+	~Texture();
+
+	void Bind(unsigned int slot);
+	void Unbind();
+
+	inline unsigned int GetSlot() { return slot; }
+};
