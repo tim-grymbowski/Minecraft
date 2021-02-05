1527ae5d35788f1252b6a8ea17f12067a8bac884
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:02:06 +0100

    Update Mesh.h

--- Source/Mesh.h
+++ Source/Mesh.h
@@ -1,1 +1,30 @@
+#pragma once
 
+#include "./Vertex.h"
+#include "./TriangleVertex.h"
+
+#include <vector>
+
+
+class Mesh
+{
+private:
+	unsigned int RendererID;
+	unsigned int VertexBufferID;
+	unsigned int IndexBufferID;
+	int VertexBufferLength;
+	int IndexBufferLength;
+public:
+	Mesh(int length);
+	Mesh(const std::vector<Vertex>& buffer);
+	Mesh(const std::vector<TriangleVertex>& buffer);
+	~Mesh();
+
+	void Data(const std::vector<Vertex>& buffer);
+	void Data(const std::vector<TriangleVertex>& buffer);
+	void AddAttribute(int index, int components, size_t offset);
+
+	inline unsigned int GetRendererID() { return RendererID; };
+	inline int GetVerticesLength() { return VertexBufferLength; };
+	inline int GetIndicesLength() { return IndexBufferLength; };
+};
