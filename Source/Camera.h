2b83a520f3b909fb6d85780d0b336809dec2a4ed
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 16:44:35 +0100

    Update Camera.h

--- Source/Camera.h
+++ Source/Camera.h
@@ -1,1 +1,17 @@
+#pragma once
 
+
+#include "./vendor/glm/glm.hpp"
+#include "./vendor/glm/gtc/matrix_transform.hpp"
+#include "./vendor/glm/gtc/type_ptr.hpp"
+
+class Camera
+{
+private:
+	glm::mat4 matrix;
+public:
+	Camera() { matrix = glm::identity<glm::mat4>(); }
+	~Camera() {}
+	inline void Update(const glm::mat4& matrix) { this->matrix *= matrix; }
+	inline glm::mat4 GetMatrix() { return matrix; }
+};
