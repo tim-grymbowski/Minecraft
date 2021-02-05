141c6455e5e4a8810df84e9fa2ab54e72adfb409
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:18:19 +0100

    Update Vertex.h

--- Source/Vertex.h
+++ Source/Vertex.h
@@ -1,1 +1,16 @@
+#pragma once
 
+struct Vertex
+{
+	float x;
+	float y;
+	float z;
+
+	float u;
+	float v;
+
+	float t;
+
+	Vertex();
+	Vertex(float x, float y, float z, float u, float v, float t);
+};
