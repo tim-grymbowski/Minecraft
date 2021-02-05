811683554165889cd43ceef8af3bcd35d6cffbab
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:16:20 +0100

    Update TriangleVertex.h

--- Source/TriangleVertex.h
+++ Source/TriangleVertex.h
@@ -1,1 +1,14 @@
+#pragma once
 
+
+struct TriangleVertex {
+	float x;
+	float y;
+	float z;
+	float r;
+	float g;
+	float b;
+
+	TriangleVertex();
+	TriangleVertex(float x, float y, float z, float r, float g, float b);
+};
