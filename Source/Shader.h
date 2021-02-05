e28de21d3c304baaa4d05f1544ababedf60844e7
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:09:44 +0100

    Update Shader.h

--- Source/Shader.h
+++ Source/Shader.h
@@ -1,1 +1,21 @@
+#pragma once
 
+#include <unordered_map>
+
+class Shader
+{
+private:
+	unsigned int RendererID;
+	std::unordered_map<std::string, unsigned int> uniforms;
+public:
+	Shader(const char* vertexSource, const char* fragmentSource);
+	~Shader();
+
+	void Bind();
+	void Unbind();
+
+	void SetMatrix(const char* matrix, const float* data);
+	void SetSampler(const char* sampler, int slot);
+
+	inline unsigned int GetID() { return RendererID; }
+};
