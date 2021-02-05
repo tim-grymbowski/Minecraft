a6ed0e2f8996df6d9f87fdcc59e2f1c12327efef
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 17:15:28 +0100

    Update TriangleVertex.cpp

--- Source/TriangleVertex.cpp
+++ Source/TriangleVertex.cpp
@@ -1,1 +1,20 @@
+#include "./TriangleVertex.h"
 
+
+TriangleVertex::TriangleVertex() {
+	this->x = 0.0f;
+	this->y = 0.0f;
+	this->z = 0.0f;
+	this->r = 0.0f;
+	this->g = 0.0f;
+	this->b = 0.0f;
+}
+
+TriangleVertex::TriangleVertex(float x, float y, float z, float r, float g, float b) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->r = r;
+	this->g = g;
+	this->b = b;
+}
