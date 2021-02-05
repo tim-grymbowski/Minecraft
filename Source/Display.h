03ba8d87d89d404547297b76fdd71e039c9c5e70
Tim Grymbowski <66548659+tim-grymbowski@users.noreply.github.com>
2021-02-05 16:50:02 +0100

    Update Display.h

--- Source/Display.h
+++ Source/Display.h
@@ -1,1 +1,22 @@
+#pragma once
 
+
+class Display
+{
+private:
+	GLFWwindow* window;
+public:
+public:
+	Display(int width, int height, const char* title);
+	~Display();
+
+	bool IsClosed();
+	void Update();
+	void Poll();
+
+	inline void SetWidth(int width) {};
+	inline void SetHeight(int height) {};
+	inline void SetTitle(const char* title) {};
+
+	inline GLFWwindow* Get() { return window; };
+};
